e62fda8ff167014cb8bfe16473f1d8362b0e6041
Evan Barger <epbarger@gmail.com>
2020-03-30 22:38:06 -0400

    Finished menu file loading, style improvements

--- script/app.js
+++ script/app.js
@@ -1,20 +1,34 @@
 let app = new Vue({
   el: '#app',
   data: {
+    menu: new Menu(),
     roms: [],
     processor: new Processor([]),
     filename: 'output',
     mapData: '',
     romData: ''
   },
+  created: function() {
+    this.processor.menu = this.menu;
+  },
   methods: {
-    addFile: async function(e) {
-      this.roms.push(new ROM(await e.target.files[0].arrayBuffer()));
+    addMenu: function(e) {
+      let fileReader = new FileReader()
+      fileReader.onload = () => this.menu.setData(fileReader.result);
+      fileReader.readAsArrayBuffer(e.target.files[0]);
+
+      e.target.value = '';
+    },
+    addROM: function(e) {
+      let fileReader = new FileReader()
+      fileReader.onload = () => this.roms.push(new ROM(fileReader.result));
+      fileReader.readAsArrayBuffer(e.target.files[0]);
+
       this.processor.roms = this.roms;
 
       e.target.value = '';
     },
-    removeFile: function(index) {
+    removeROM: function(index) {
       this.roms.splice(index, 1);
       this.processor.roms = this.roms;
     },
@@ -37,6 +51,9 @@ let app = new Vue({
     downloadRomFile: function(e) {
       if (this.romData) { URL.revokeObjectURL(this.romData) }
       this.romData = URL.createObjectURL(new Blob([this.processor.romData()]));
+    },
+    downloadEnabled: function() {
+      return (this.roms.length > 0) && this.menu.present() && this.menu.valid() ;
     }
   }
 });
